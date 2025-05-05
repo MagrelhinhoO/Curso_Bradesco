b90846b0ddf4436c01655f7e183ae76aa836af15
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-05-05 19:54:46 -0300

    Metodo selecionar candidatos

--- src/ControleCandidatos/candidatura/ProcessoSeletivo.java
+++ src/ControleCandidatos/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,32 @@
 package ControleCandidatos.candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo seletivo");
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"ARTHUR","JOÃO","SILVA","PEDRO","CLEMENTINA","VAN HELLSING","lADY GAGA","CELLBIT","MURILO","BLUEZÃO"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.printf("O candidato "+ candidato +" solicitou este valor de salário: R$"+ salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato "+ candidato +" foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
